9dfd0201a4f78ff29b708733914e62d4db43f9ac
HasanColakovic99 <hasancolakovic32@gmail.com>
2022-04-17 11:09:50 +0200

    New details commited

--- GuessMyNumberGame/script.js
+++ GuessMyNumberGame/script.js
@@ -1,21 +1,10 @@
 'use strict';
 
-
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-
-
-// document.querySelector('.guess').value = 34;
-// console.log(document.querySelector('.guess').value);
-
-
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 
-const btn = document.querySelector('.check');
+const btnCheck = document.querySelector('.check');
 const buttonCheck = function () {
     const guess = Number(document.querySelector('.guess').value);
 
@@ -28,6 +17,11 @@ const buttonCheck = function () {
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
 
+
+        highscore = document.querySelector('.highscore').textContent;
+        if (score > highscore) {
+            document.querySelector('.highscore').textContent = score;
+        }
     }
     else if (guess > secretNumber) {
         if (score > 1) {
@@ -52,5 +46,27 @@ const buttonCheck = function () {
         }
     }
 }
+btnCheck.addEventListener('click', buttonCheck);
 
-btn.addEventListener('click', buttonCheck);
+
+const btnAgain = document.querySelector('.again');
+const again = function () {
+
+    score = 20;
+
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+    document.querySelector('.guess').value = " ";
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+
+    document.querySelector('.number').textContent = '?';
+
+    document.querySelector('.number').style.width = '15rem';
+
+    document.querySelector('.score').textContent = score;
+
+    document.querySelector('body').style.backgroundColor = '#222';
+}
+
+btnAgain.addEventListener('click', again);
