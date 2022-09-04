defddef071498b62c0e9655d68640f58192fdd8a
Hasan Čolaković <hasancolakovic32@gmail.com>
2022-09-04 13:23:51 +0200

    Finished dad joke with api project

--- dad jokes api project/script.js
+++ dad jokes api project/script.js
@@ -1,32 +1,30 @@
-const jokeEl = document.getElementById('joke');
-const jokeBtn = document.getElementById('jokeBtn');
+const joke = document.getElementById('joke');
+const btn = document.getElementById('jokeBtn');
 
-generateJoke()
+btn.addEventListener('click', () => {
+    generateJoke();
+})
 
-jokeBtn.addEventListener('click', generateJoke);
-
-async function generateJoke() {
-    const config = {
-        headers: {
-            Accept: 'application/json',
-        },
-    }
-    // RAD S API-om
-    const res = await fetch('https://icanhazdadjoke.com', config)
-
-    const data = await res.json();
-
-    jokeEl.innerHTML = data.joke;
-}
+generateJoke();
 
 // function generateJoke() {
-//     const config = {
+//     fetch('https://icanhazdadjoke.com', {
 //         headers: {
-//             Accept: 'application/json',
-//         },
-//     }
-//     // RAD S API-om
-//     fetch('https://icanhazdadjoke.com', config).then((response) =>   response.json()).then((data) => {
-//         jokeEl.innerHTML = data.joke
+//             'Accept': 'application/json'
+//         }
 //     })
-// }
+//     .then(response => response.json())
+//     .then(data => {
+//         joke.innerHTML = data.joke
+//     });
+// };
+
+async function generateJoke() {
+    const res = await fetch('https://icanhazdadjoke.com', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+    const data = await res.json();
+    joke.innerHTML = data.joke;
+};
